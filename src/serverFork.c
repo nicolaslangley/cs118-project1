358c0d625c2c2b6b69fa21f8abbe874501117fa1
Nicolas Langley <nicolasqlangley@gmail.com>
2015-05-02 12:25:47 -0700

    Fixed spacing issue

--- src/serverFork.c
+++ src/serverFork.c
@@ -27,61 +27,63 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, pid;
-     socklen_t clilen;
-     struct sockaddr_in serv_addr, cli_addr;
-     struct sigaction sa;          // for signal SIGCHLD
 
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     
-     listen(sockfd,5);
-     
-     clilen = sizeof(cli_addr);
-     
-     /****** Kill Zombie Processes ******/
-     sa.sa_handler = sigchld_handler; // reap all dead processes
-     sigemptyset(&sa.sa_mask);
-     sa.sa_flags = SA_RESTART;
-     if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-         perror("sigaction");
-         exit(1);
-     }
-     /*********************************/
-     
-     while (1) {
-         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-         
-         if (newsockfd < 0) 
-             error("ERROR on accept");
-         
-         pid = fork(); //create a new process
-         if (pid < 0)
-             error("ERROR on fork");
-         
-         if (pid == 0)  { // fork() returns a value of 0 to the child process
-             close(sockfd);
-             dostuff(newsockfd);
-             exit(0);
-         }
-         else //returns the process ID of the child process to the parent
-             close(newsockfd); // parent doesn't need this 
-     } /* end of while */
-     return 0; /* we never get here */
+    
+    int sockfd, newsockfd, portno, pid;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+    struct sigaction sa;          // for signal SIGCHLD
+
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+       error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+             sizeof(serv_addr)) < 0) 
+             error("ERROR on binding");
+    
+    listen(sockfd,5);
+    
+    clilen = sizeof(cli_addr);
+    
+    /****** Kill Zombie Processes ******/
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+    /*********************************/
+    
+    while (1) {
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        
+        if (newsockfd < 0) 
+            error("ERROR on accept");
+        
+        pid = fork(); //create a new process
+        if (pid < 0)
+            error("ERROR on fork");
+        
+        if (pid == 0)  { // fork() returns a value of 0 to the child process
+            close(sockfd);
+            dostuff(newsockfd);
+            exit(0);
+        }
+        else //returns the process ID of the child process to the parent
+            close(newsockfd); // parent doesn't need this 
+    } /* end of while */
+    return 0; /* we never get here */
 }
 
 /******** DOSTUFF() *********************
@@ -91,13 +93,14 @@ int main(int argc, char *argv[])
  *****************************************/
 void dostuff (int sock)
 {
-   int n;
-   char buffer[256];
-      
-   bzero(buffer,256);
-   n = read(sock,buffer,255);
-   if (n < 0) error("ERROR reading from socket");
-   printf("Here is the message: %s\n",buffer);
-   n = write(sock,"I got your message",18);
-   if (n < 0) error("ERROR writing to socket");
+
+    int n;
+    char buffer[256];
+       
+    bzero(buffer,256);
+    n = read(sock,buffer,255);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n",buffer);
+    n = write(sock,"I got your message",18);
+    if (n < 0) error("ERROR writing to socket");
 }
