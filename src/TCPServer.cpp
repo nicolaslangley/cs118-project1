c7f7abb16204e0ecf4b17ec6118567bbcd5db9c5
Nicolas Langley <nicolasqlangley@gmail.com>
2015-05-02 11:56:53 -0700

    First commit

---
+++ src/TCPServer.cpp
@@ -0,0 +1,12 @@
+#include <sys/sockets.h> 
+#include <iostream.h>
+
+void main()
+{
+    // Step 1: Setup the Server Port and Socket
+    // Step 2: Wait for incoming connection request
+    // Step 3: Read request from the socket
+    // Step 4: Reply to socket
+    // Step 5: Close the socket
+    std::cout << "TCP Server" << std::endl;
+}
